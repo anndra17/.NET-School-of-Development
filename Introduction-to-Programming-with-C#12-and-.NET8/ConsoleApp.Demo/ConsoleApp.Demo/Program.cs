a3d212a0c20ddd5ffc6490a7ea3c63d321938c2e
anndra17 <andra.hor@icloud.com>
2025-10-09 14:01:59 +0300

    initialCommit

---
+++ Introduction-to-Programming-with-C#12-and-.NET8/ConsoleApp.Demo/ConsoleApp.Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿// Variable Declarations and Types
+
+string? firstName = string.Empty;
+string? lastName = string.Empty;
+int age; // default 0
+int retirementAge = 65;
+decimal salary; // default 0.0m
+char gender; // default char.MinValue;
+bool working; // by default false
+
+// Prompt the user for imput
+Console.Write("Please eneter your first name: ");
+firstName = Console.ReadLine();
+
+Console.Write("Please eneter your last name: ");
+lastName = Console.ReadLine();
+
+Console.Write("Please eneter your age: ");
+age = int.Parse(Console.ReadLine());
+
+Console.Write("Please eneter your salary: ");
+salary = decimal.Parse(Console.ReadLine());
+
+Console.Write("Please eneter your gender (M or F): ");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Are you currently working? (true or false): ");
+working = Convert.ToBoolean(Console.ReadLine());
+
+// Process the data
+int workingYearsRemaining = retirementAge - age;
+
+// Output the results to the user
+Console.WriteLine($"Hello {firstName} {lastName}, you have {workingYearsRemaining} years left until you reach the age of {retirementAge}.");
+Console.WriteLine($"Your salary is {salary:C}, \0 You are employed: {working.ToString()}.");
+Console.WriteLine($"Your gender is {gender}");

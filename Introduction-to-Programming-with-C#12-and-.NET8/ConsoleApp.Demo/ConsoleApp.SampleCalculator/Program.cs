a854634a727110b1d67cf1a6893e7ab2ec79fc72
Andra Horhocea <andra.hor@icloud.com>
2025-10-10 13:33:14 +0300

    Complete simple calculator app

---
+++ Introduction-to-Programming-with-C#12-and-.NET8/ConsoleApp.Demo/ConsoleApp.SampleCalculator/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Welcome Message
+Console.WriteLine("********** - Welcome to the sample calculator! - **********");
+
+// Prompt for user input
+Console.WriteLine("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Show calculator options / Show menu
+Console.WriteLine("Please select an operation");
+Console.WriteLine("1 - Addition");
+Console.WriteLine("2 - Subtraction");
+Console.WriteLine("3 - Multiplication");
+Console.WriteLine("4 - Division");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+// Decide which operation is needed based on selected option
+int answer = 0;
+switch (choice)
+{
+    case 1:
+        // Addition
+        answer = num1 + num2;
+        break;
+    case 2:
+        // Subtraction
+        answer = num1 - num2;
+        break;
+    case 3:
+        // Multiplication
+        answer = num1 * num2;
+        break;
+    case 4:
+        // Division
+        answer = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Invalid choice");
+        break;
+}
+
+// Print output
+Console.WriteLine($"The answer is {answer}");

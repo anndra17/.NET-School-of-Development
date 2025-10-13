f9018099815d19996d4242ccee59a60b3f1785d9
Andra Horhocea <andra.hor@icloud.com>
2025-10-13 15:50:05 +0300

    Coding exercise - Add Exception Handling to the Calculator - Part1

--- Introduction-to-Programming-with-C#12-and-.NET8/ConsoleApp.Demo/ConsoleApp.SampleCalculator/Program.cs
+++ Introduction-to-Programming-with-C#12-and-.NET8/ConsoleApp.Demo/ConsoleApp.SampleCalculator/Program.cs
@@ -3,45 +3,100 @@
 // Welcome Message
 Console.WriteLine("********** - Welcome to the sample calculator! - **********");
 
-// Prompt for user input
-Console.WriteLine("Please enter the first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Please enter the second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Variable Declarations
+int choice = 0;
+int num1, num2 = 0;
 
 // Show calculator options / Show menu
-Console.WriteLine("Please select an operation");
-Console.WriteLine("1 - Addition");
-Console.WriteLine("2 - Subtraction");
-Console.WriteLine("3 - Multiplication");
-Console.WriteLine("4 - Division");
-int choice = Convert.ToInt32(Console.ReadLine());
-
-// Decide which operation is needed based on selected option
-int answer = 0;
-switch (choice)
+while(choice != -1)
 {
-    case 1:
-        // Addition
-        answer = num1 + num2;
-        break;
-    case 2:
-        // Subtraction
-        answer = num1 - num2;
-        break;
-    case 3:
-        // Multiplication
-        answer = num1 * num2;
-        break;
-    case 4:
-        // Division
-        answer = num1 / num2;
-        break;
-    default:
-        Console.WriteLine("Invalid choice");
-        break;
+    Console.WriteLine("Please select an operation (-1 to exit program)");
+    Console.WriteLine("1 - Addition");
+    Console.WriteLine("2 - Subtraction");
+    Console.WriteLine("3 - Multiplication");
+    Console.WriteLine("4 - Division");
+    Console.WriteLine("5. Fibonacci sequence");
+
+    try
+    {
+        choice = Convert.ToInt32(Console.ReadLine());
+    }
+    catch(Exception)
+    {
+        Console.WriteLine("Invalid input, press any key to try again. ");
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
+
+    // Prompt for user input
+    try
+    {
+        Console.WriteLine("Please enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+    }
+    catch(Exception)
+    {
+        Console.WriteLine("Invalid input, press any key to try again. ");
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
+
+    // Decide which operation is needed based on selected option
+    int answer = 0;
+    switch (choice)
+    {
+        case 1:
+            // Addition
+            answer = num1 + num2;
+            break;
+        case 2:
+            // Subtraction
+            answer = num1 - num2;
+            break;
+        case 3:
+            // Multiplication
+            answer = num1 * num2;
+            break;
+        case 4:
+            // Division
+            try
+            {
+                answer = num1 / num2;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero!");
+                Console.WriteLine("Press Enter to continue");
+                Console.ReadLine();
+                Console.Clear();
+                continue;
+            }
+            break;
+        case 5:
+            // Fibonacci 
+            for (int i = num1; i <= num2; i++)
+                answer += i;
+            break;
+        default:
+            Console.WriteLine("Invalid choice");
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            continue;
+    }
+
+    // Print output
+    Console.WriteLine($"The answer is {answer}");
+    Console.WriteLine("Press Enter to continue");
+    Console.ReadLine();
+    Console.Clear();
 }
 
-// Print output
-Console.WriteLine($"The answer is {answer}");
+
+
+
--- Introduction-to-Programming-with-C#12-and-.NET8/ConsoleApp.Demo/ConsoleApp.SampleCalculator/obj/Debug/net8.0/ConsoleApp.SampleCalculator.AssemblyInfo.cs
+++ Introduction-to-Programming-with-C#12-and-.NET8/ConsoleApp.Demo/ConsoleApp.SampleCalculator/obj/Debug/net8.0/ConsoleApp.SampleCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp.SampleCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf76b33702d648b54b3a502d2d58c8c43ebf59a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f4378fb39cdbd0a9907bb79a94a0b4f603531b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp.SampleCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp.SampleCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

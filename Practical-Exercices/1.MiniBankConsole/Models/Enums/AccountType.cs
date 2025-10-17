beedc6d4f729077b74926143245e62078a94da09
Andra Horhocea <andra.hor@icloud.com>
2025-10-17 10:23:15 +0300

    feat: add accountType enum

---
+++ Practical-Exercices/1.MiniBankConsole/Models/Enums/AccountType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBankConsole.Models.Enums
+{
+    public enum AccountType
+    {
+        Checking = 1,
+        Savings = 2,
+        Loan = 3
+    }
+}

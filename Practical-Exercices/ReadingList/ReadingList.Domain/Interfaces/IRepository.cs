17a208cb5d7bc72f43bc21865a17f66ca18d37d7
Andra Horhocea <andra.hor@icloud.com>
2025-10-31 11:41:17 +0200

    feat(domain): define IRepository and optional IUnitOfWork abstractions

---
+++ Practical-Exercices/ReadingList/ReadingList.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingList.Domain.Interfaces
+{
+    public interface IRepository<T, TKey>
+        where TKey : notnull
+        where T : class
+    {
+        bool Add(T entity);
+        bool Upsert(T entity);
+        bool Remove(TKey id);
+        bool TryGet(TKey id, out T? entity);
+        IReadOnlyCollection<T> GetAll();
+
+        int Count {  get; }
+    }
+}

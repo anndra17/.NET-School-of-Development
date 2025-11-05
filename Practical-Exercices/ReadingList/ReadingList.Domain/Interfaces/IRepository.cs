8aef842d8baa069b68e7e008f33c793737a4f1c6
Andra Horhocea <andra.hor@icloud.com>
2025-11-05 16:35:46 +0200

    style(domain): remove unused usings and migrate to file-scoped namespaces

--- Practical-Exercices/ReadingList/ReadingList.Domain/Interfaces/IRepository.cs
+++ Practical-Exercices/ReadingList/ReadingList.Domain/Interfaces/IRepository.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ReadingList.Domain.Interfaces;
 
-namespace ReadingList.Domain.Interfaces
+public interface IRepository<T, TKey>
+    where TKey : notnull
+    where T : class
 {
-    public interface IRepository<T, TKey>
-        where TKey : notnull
-        where T : class
-    {
-        bool Add(T entity);
-        bool Upsert(T entity);
-        bool Remove(TKey id);
-        bool TryGet(TKey id, out T? entity);
-        IReadOnlyCollection<T> GetAll();
+    bool Add(T entity);
+    bool Upsert(T entity);
+    bool Remove(TKey id);
+    bool TryGet(TKey id, out T? entity);
+    IReadOnlyCollection<T> GetAll();
 
-        int Count {  get; }
-    }
+    int Count {  get; }
 }

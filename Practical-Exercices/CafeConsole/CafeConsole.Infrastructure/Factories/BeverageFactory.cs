dbdec4cbf4f851fa0ae252be37653afe833fc53f
Andra Horhocea <andra.hor@icloud.com>
2025-11-12 17:24:35 +0200

    feat(infra): add BeverageFactory implementation

---
+++ Practical-Exercices/CafeConsole/CafeConsole.Infrastructure/Factories/BeverageFactory.cs
@@ -0,0 +1,33 @@
+﻿using CafeConsole.App.Abstractions;
+using CafeConsole.Domain.Abstractions;
+using CafeConsole.Domain.Models.Base;
+
+namespace CafeConsole.Infrastructure.Factories;
+
+public class BeverageFactory : IBeverageFactory
+{
+    private readonly Dictionary<string, Func<IBeverage>> _creators = new Dictionary<string, Func<IBeverage>>();
+
+    public BeverageFactory() 
+    {
+        _creators["espresso"] = () => new Espresso();
+        _creators["tea"] = () => new Tea();
+        _creators["hot chocolate"] = () => new HotChocolate();
+    }
+
+    public IBeverage Create(string type)
+    {
+        Func<IBeverage> creator;
+
+        _creators.TryGetValue(type, out creator);
+
+        if (creator == null)
+        {
+            throw new Exception($"Unknown beverage: {type}");
+        }
+
+        IBeverage beverage= creator();
+
+        return beverage;
+    }
+}

5d4052be102bb889cc2dfa5d9b6b8139aa1129ce
Andra Horhocea <andra.hor@icloud.com>
2025-11-14 12:03:35 +0200

    feat(events): add OrderPlaced event record

---
+++ Practical-Exercices/CafeConsole/CafeConsole.Domain/Models/Events/OrderPlaced.cs
@@ -0,0 +1,9 @@
+﻿namespace CafeConsole.Domain.Models.Events;
+
+public readonly record struct OrderPlaced(
+    Guid Id,
+    DateTimeOffset At,
+    string Description,
+    decimal Subtotal,
+    decimal Total
+    );

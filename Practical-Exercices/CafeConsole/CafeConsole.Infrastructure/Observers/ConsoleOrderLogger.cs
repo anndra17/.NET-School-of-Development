c7daa6e601b0e539f9163b8dd96dbb614085f4f5
Andra Horhocea <andra.hor@icloud.com>
2025-11-14 12:11:26 +0200

    feat(events): add order observers
    Add ConsoleOrderLogger and InMemoryOrderAnalytics observers in infrastructure.

---
+++ Practical-Exercices/CafeConsole/CafeConsole.Infrastructure/Observers/ConsoleOrderLogger.cs
@@ -0,0 +1,19 @@
+﻿using CafeConsole.Domain.Abstractions;
+using CafeConsole.Domain.Models.Events;
+
+namespace CafeConsole.Infrastructure.Observers;
+
+public class ConsoleOrderLogger : IOrderEventSubscriber
+{
+    public void On(OrderPlaced orderEvent)
+    {
+        Console.WriteLine($"""
+            Order {orderEvent.Id} @ {orderEvent.At}
+            Items: {orderEvent.Description}
+            Subtotal: {orderEvent.Subtotal.ToString("C")}
+            Pricing: TODO
+            Total: {orderEvent.Total.ToString("C")}";
+            """);
+    }
+
+}
---
+++ Practical-Exercices/CafeConsole/CafeConsole.Infrastructure/Observers/InMemoryOrderAnalytics.cs
@@ -0,0 +1,16 @@
+﻿using CafeConsole.Domain.Abstractions;
+using CafeConsole.Domain.Models.Events;
+
+namespace CafeConsole.Infrastructure.Observers;
+
+public class InMemoryOrderAnalytics : IOrderEventSubscriber
+{
+    public int OrdersCount { get; private set; }
+    public decimal TotalRevenue { get; private set; }
+
+    public void On(OrderPlaced orderEvent)
+    {
+        OrdersCount++;
+        TotalRevenue += orderEvent.Total;
+    }
+}

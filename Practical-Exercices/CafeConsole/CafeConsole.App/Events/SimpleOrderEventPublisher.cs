6714bfee6608467478e516843171810c3db80872
Andra Horhocea <andra.hor@icloud.com>
2025-11-14 12:07:42 +0200

    feat(events): add SimpleOrderEventPublisher
    Add SimpleOrderEventPublisher to publish order events from the application layer.

---
+++ Practical-Exercices/CafeConsole/CafeConsole.App/Events/SimpleOrderEventPublisher.cs
@@ -0,0 +1,22 @@
+﻿using CafeConsole.Domain.Abstractions;
+using CafeConsole.Domain.Models.Events;
+
+namespace CafeConsole.App.Events;
+
+public class SimpleOrderEventPublisher : IOrderEventPublisher
+{
+    private readonly IEnumerable<IOrderEventSubscriber> _subscribers;
+    
+    public SimpleOrderEventPublisher(IEnumerable<IOrderEventSubscriber> subscribers)
+    {
+        _subscribers = subscribers;
+    }
+
+    public void Publish(OrderPlaced orderEvent)
+    {
+        foreach (var subscriber in _subscribers) 
+        {
+            subscriber.On(orderEvent);
+        }
+    }
+}

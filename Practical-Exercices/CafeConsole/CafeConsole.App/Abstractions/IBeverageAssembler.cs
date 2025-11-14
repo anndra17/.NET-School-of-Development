78bc63bc31038d0afd96f46cd57278d72e8a3b59
Andra Horhocea <andra.hor@icloud.com>
2025-11-14 19:14:28 +0200

    feat(app): add IBeverageAssembler interface

---
+++ Practical-Exercices/CafeConsole/CafeConsole.App/Abstractions/IBeverageAssembler.cs
@@ -0,0 +1,8 @@
+﻿using CafeConsole.Domain.Abstractions;
+
+namespace CafeConsole.App.Abstractions;
+
+public interface IBeverageAssembler
+{
+    IBeverage Assemble(IBeverage baseType, IEnumerable<(string Name, string? flavor)> addOns);
+}

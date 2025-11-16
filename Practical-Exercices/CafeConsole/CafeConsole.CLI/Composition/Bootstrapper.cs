633b4fc50688ac416c6588a9baf4c1f10e183aba
Andra Horhocea <andra.hor@icloud.com>
2025-11-16 17:51:59 +0200

    feat(cli): wire IPricingStrategy implementations and provide selector to OrderService

--- Practical-Exercices/CafeConsole/CafeConsole.CLI/Composition/Bootstrapper.cs
+++ Practical-Exercices/CafeConsole/CafeConsole.CLI/Composition/Bootstrapper.cs
@@ -1,7 +1,10 @@
 ﻿using CafeConsole.App.Abstractions;
 using CafeConsole.App.Events;
+using CafeConsole.App.Selector;
 using CafeConsole.App.Services;
 using CafeConsole.CLI.Menus;
+using CafeConsole.Domain.Abstractions;
+using CafeConsole.Domain.Models.Pricing;
 using CafeConsole.Infrastructure.Composition;
 using CafeConsole.Infrastructure.Factories;
 using CafeConsole.Infrastructure.Observers;
@@ -12,23 +15,34 @@ public static class Bootstrapper
 {
     public static Task RunAsync()
     {
-        var publisher = new SimpleOrderEventPublisher();
         var logger = new ConsoleOrderLogger();
         var analytics = new InMemoryOrderAnalytics();
+
+        var publisher = new SimpleOrderEventPublisher();
         publisher.Register(logger);
         publisher.Register(analytics);
 
         IBeverageFactory beverageFactory = new BeverageFactory();
         IBeverageAssembler beverageAssembler = new BeverageAssembler();
-        var orderService = new OrderService(publisher);
+
+        IPricingStrategy[] strategies =
+        {
+            new RegularPricing(),
+            new HappyHourPricing()
+        };
+        IPricingStrategySelector selector = new PricingStrategySelector(strategies);
+
+        var orderService = new OrderService(publisher, selector);
 
         var beverageMenu = new BeverageMenu(beverageFactory);
         var addOnMenu = new AddOnMenu(beverageAssembler);
         var pricingMenu = new PricingMenu();
         var receiptMenu = new ReceiptMenu(orderService);
+        
         var menu = new Menu(beverageMenu, addOnMenu, pricingMenu, receiptMenu);
-       
         menu.Run();
+
+
         return Task.CompletedTask;
     }
 }

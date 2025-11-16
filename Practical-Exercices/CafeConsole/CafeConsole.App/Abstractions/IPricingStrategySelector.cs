840ce4c665813841f58d84940b9f7650e1a50221
Andra Horhocea <andra.hor@icloud.com>
2025-11-16 16:45:13 +0200

    feat(app): add IPricingStrategySelector

---
+++ Practical-Exercices/CafeConsole/CafeConsole.App/Abstractions/IPricingStrategySelector.cs
@@ -0,0 +1,9 @@
+﻿using CafeConsole.Domain.Abstractions;
+using CafeConsole.Domain.Models.Pricing;
+
+namespace CafeConsole.App.Abstractions;
+
+public interface IPricingStrategySelector
+{
+    IPricingStrategy Resolve(PricingPolicy policy);
+}

3ca00582c44f7dfca690404971943762ff5854b0
Andra Horhocea <andra.hor@icloud.com>
2025-11-16 17:10:32 +0200

    feat(domain): add PricingPolicy member in OrderPlaced

--- Practical-Exercices/CafeConsole/CafeConsole.Domain/Models/Events/OrderPlaced.cs
+++ Practical-Exercices/CafeConsole/CafeConsole.Domain/Models/Events/OrderPlaced.cs
@@ -1,9 +1,12 @@
-﻿namespace CafeConsole.Domain.Models.Events;
+﻿using CafeConsole.Domain.Models.Pricing;
+
+namespace CafeConsole.Domain.Models.Events;
 
 public readonly record struct OrderPlaced(
     Guid Id,
     DateTimeOffset At,
     string Description,
     decimal Subtotal,
+    PricingPolicy Policy,
     decimal Total
     );

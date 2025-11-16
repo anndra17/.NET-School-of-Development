ff6002a5fcb450e4ec685ca3b72a0b13c65a84b1
Andra Horhocea <andra.hor@icloud.com>
2025-11-16 19:22:37 +0200

    refactor(infra): seal order InMemoryOrderAnalytics class

--- Practical-Exercices/CafeConsole/CafeConsole.Infrastructure/Observers/InMemoryOrderAnalytics.cs
+++ Practical-Exercices/CafeConsole/CafeConsole.Infrastructure/Observers/InMemoryOrderAnalytics.cs
@@ -3,7 +3,7 @@ using CafeConsole.Domain.Models.Events;
 
 namespace CafeConsole.Infrastructure.Observers;
 
-public class InMemoryOrderAnalytics : IOrderEventSubscriber
+public sealed class InMemoryOrderAnalytics : IOrderEventSubscriber
 {
     public int OrdersCount { get; private set; }
     public decimal TotalRevenue { get; private set; }

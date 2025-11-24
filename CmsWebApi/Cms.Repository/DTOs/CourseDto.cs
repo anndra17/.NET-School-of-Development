6455a03816f894fddf34ca12be7c13601a66032e
anndra17 <andra.horhocea@student.unitbv.ro>
2025-11-24 18:21:16 +0200

    feat: add Course DTO

---
+++ CmsWebApi/Cms.Repository/DTOs/CourseDto.cs
@@ -0,0 +1,11 @@
+﻿using Cms.Repository.Models;
+
+namespace Cms.Repository.DTOs;
+
+public class CourseDto
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    public int CourseDuration { get; set; }
+    public COURSE_TYPE CourseType { get; set; }
+}

0c6f8c0e4049bc99917d44679fff08e7382fd086
anndra17 <andra.horhocea@student.unitbv.ro>
2025-11-24 14:55:04 +0200

    feat: add new CourseController

---
+++ CmsWebApi/Cms.WebApi/Controllers/CoursesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cms.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CoursesController : ControllerBase
+{
+    public CoursesController()
+    {
+
+    }
+
+    [HttpGet]
+    public IEnumerable<Course> GetCoureses()
+    {
+        //return "This will return all courses";
+    }
+}

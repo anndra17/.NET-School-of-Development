4496a7e994463ff57986c86f5b9b30bc322b9eab
anndra17 <andra.horhocea@student.unitbv.ro>
2025-11-24 15:14:24 +0200

    feat: add SqlCmsRepository class

---
+++ CmsWebApi/Cms.Repository/Repositories/SqlCmsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Cms.Repository.Models;
+
+namespace Cms.Repository.Repositories;
+
+public class SqlCmsRepository : ICmsRepository
+{
+    public SqlCmsRepository()
+    {
+    }
+
+    public IEnumerable<Course> GetAllCourses()
+    {
+        return null;
+    }
+}

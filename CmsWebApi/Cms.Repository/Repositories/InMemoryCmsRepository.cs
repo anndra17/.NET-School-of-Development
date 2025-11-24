0e51d67a148a78c77ebdc87775b50928f0578ddf
anndra17 <andra.horhocea@student.unitbv.ro>
2025-11-24 18:20:32 +0200

    feat: add hardcoded courses to work with in InMemoryRepository

--- CmsWebApi/Cms.Repository/Repositories/InMemoryCmsRepository.cs
+++ CmsWebApi/Cms.Repository/Repositories/InMemoryCmsRepository.cs
@@ -4,13 +4,58 @@ namespace Cms.Repository.Repositories;
 
 public class InMemoryCmsRepository : ICmsRepository
 {
+    List<Course> _courses = null;
     public InMemoryCmsRepository()
     {
-
+        _courses = new List<Course>();
+        _courses.Add(
+            new Course() 
+            { 
+                CourseId = 1, 
+                CourseName = "Computer Science", 
+                CourseDuration = 4, 
+                CourseType = COURSE_TYPE.ENIGINEERING 
+            }
+        );
+        _courses.Add(
+            new Course() { 
+                CourseId = 2, 
+                CourseName = "Mechanical Engineering", 
+                CourseDuration = 4, 
+                CourseType = COURSE_TYPE.ENIGINEERING 
+            }
+        );
+        _courses.Add(
+            new Course() 
+            {   
+                CourseId = 3, 
+                CourseName = "Civil Engineering", 
+                CourseDuration = 4, 
+                CourseType = COURSE_TYPE.ENIGINEERING 
+            }
+        );
+        _courses.Add(
+            new Course() 
+            { 
+                CourseId = 4, 
+                CourseName = "MBBS", 
+                CourseDuration = 5, 
+                CourseType = COURSE_TYPE.MEDICAL 
+            }
+        );
+        _courses.Add(
+            new Course() 
+            { 
+                CourseId = 5, 
+                CourseName = "BDS", 
+                CourseDuration = 5, 
+                CourseType = COURSE_TYPE.MEDICAL 
+            }
+        );
     }
 
     public IEnumerable<Course>  GetAllCourses()
     {
-        return null;
+        return _courses;
     }
 }

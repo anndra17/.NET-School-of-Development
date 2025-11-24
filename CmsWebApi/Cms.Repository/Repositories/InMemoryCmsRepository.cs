81334f8556b2722ef01c400cbfa4d6b2dabac1f2
anndra17 <andra.horhocea@student.unitbv.ro>
2025-11-24 15:13:41 +0200

    feat: add InMemoryRepository class

---
+++ CmsWebApi/Cms.Repository/Repositories/InMemoryCmsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Cms.Repository.Models;
+
+namespace Cms.Repository.Repositories; 
+
+public class InMemoryCmsRepository : ICmsRepository
+{
+    public InMemoryCmsRepository()
+    {
+
+    }
+
+    public IEnumerable<Course>  GetAllCourses()
+    {
+        return null;
+    }
+}

73d50dab95bcf905276b95ba6c43a8580ca7b59b
anndra17 <andra.horhocea@student.unitbv.ro>
2025-11-24 18:22:19 +0200

    feat: modify the GetCourses method to return the courses from repository by using mapper method

--- CmsWebApi/Cms.WebApi/Controllers/CoursesController.cs
+++ CmsWebApi/Cms.WebApi/Controllers/CoursesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cms.Repository.DTOs;
+using Cms.Repository.Models;
+using Cms.Repository.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Cms.WebApi.Controllers;
 
@@ -6,14 +10,61 @@ namespace Cms.WebApi.Controllers;
 [Route("[controller]")]
 public class CoursesController : ControllerBase
 {
-    public CoursesController()
-    {
+    private readonly ICmsRepository _repository;
 
+    public CoursesController(ICmsRepository repository)
+    {
+        _repository = repository;
     }
 
+    //Approach 1: Using LINQ Select to map Course to CourseDto
+    //[HttpGet]
+    //public IEnumerable<Course> GetCoureses()
+    //{
+    //    return _repository.GetAllCourses();
+    //}
+
     [HttpGet]
-    public IEnumerable<Course> GetCoureses()
+    public IEnumerable<CourseDto> GetCoureses()
     {
-        //return "This will return all courses";
+        try
+        {
+            IEnumerable<Course> courses = _repository.GetAllCourses();
+
+            var result = MapCourseToCourseDto(courses);
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+
+    //Custom mapper functions
+    private CourseDto MapCourseToCourseDto(Course course)
+    {
+        return new CourseDto()
+        {
+            CourseId = course.CourseId,
+            CourseName = course.CourseName,
+            CourseDuration = course.CourseDuration,
+            CourseType = course.CourseType
+        };
+    }
+
+    private IEnumerable<CourseDto> MapCourseToCourseDto(IEnumerable<Course> course)
+    {
+        IEnumerable<CourseDto> result;
+
+        result = course.Select(c => new CourseDto() 
+        { 
+            CourseId = c.CourseId,
+            CourseName = c.CourseName,
+            CourseDuration = c.CourseDuration,
+            CourseType = c.CourseType
+        });
+
+        return result;
     }
 }

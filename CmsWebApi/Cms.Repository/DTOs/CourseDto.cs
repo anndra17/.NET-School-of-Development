6862726f2cc1d9673f273ae6497d6949128e4da6
Andra Horhocea <andra.hor@icloud.com>
2025-11-25 16:49:56 +0200

    feat: add DTO validation

--- CmsWebApi/Cms.Repository/DTOs/CourseDto.cs
+++ CmsWebApi/Cms.Repository/DTOs/CourseDto.cs
@@ -1,4 +1,5 @@
 ﻿using Cms.Repository.Models;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Cms.Repository.DTOs;
@@ -6,8 +7,16 @@ namespace Cms.Repository.DTOs;
 public class CourseDto
 {
     public int CourseId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
     public string CourseName { get; set; }
+
+    [Required]
+    [Range(1, 5)]
     public int CourseDuration { get; set; }
+
+    [Required]
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public COURSE_TYPE CourseType { get; set; }
 }

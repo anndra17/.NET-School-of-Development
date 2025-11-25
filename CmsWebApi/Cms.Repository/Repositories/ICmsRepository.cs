e80404119dc52be8c723437749d18cd9e30532b2
Andra Horhocea <andra.hor@icloud.com>
2025-11-25 17:40:39 +0200

    feat: add UpdateCourse, IfCourseExists and GetCourseById methods definitions

--- CmsWebApi/Cms.Repository/Repositories/ICmsRepository.cs
+++ CmsWebApi/Cms.Repository/Repositories/ICmsRepository.cs
@@ -7,5 +7,7 @@ public interface ICmsRepository
     IEnumerable<Course> GetAllCourses();
     Task<IEnumerable<Course>> GetAllCoursesAsync();
     Course AddCourse(Course newCourse);
-
+    bool IfCourseExists(int courseId);
+    Course GetCourseById(int courseId);
+    Course UpdateCourse(int courseId, Course course);
 }

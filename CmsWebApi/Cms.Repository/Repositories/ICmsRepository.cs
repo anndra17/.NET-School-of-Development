41e0ef8c1c0985036ad7027dd7eae8212e31c875
Andra Horhocea <andra.hor@icloud.com>
2025-11-26 09:58:23 +0200

    feat: add associative methods declaration

--- CmsWebApi/Cms.Repository/Repositories/ICmsRepository.cs
+++ CmsWebApi/Cms.Repository/Repositories/ICmsRepository.cs
@@ -4,11 +4,18 @@ namespace Cms.Repository.Repositories;
 
 public interface ICmsRepository
 {
+    // Collection
     IEnumerable<Course> GetAllCourses();
     Task<IEnumerable<Course>> GetAllCoursesAsync();
     Course AddCourse(Course newCourse);
     bool IfCourseExists(int courseId);
+
+    // Individual item
     Course GetCourseById(int courseId);
     Course UpdateCourse(int courseId, Course course);
     Course RemoveCourse(int courseId);
+
+    // Association
+    IEnumerable<Student> GetStudents(int courseId);
+    //Student AddStudent(int courseId, Student student);
 }

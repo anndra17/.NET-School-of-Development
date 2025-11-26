26a81280727950ab68e5e2e68ec22f5defef1aaa
Andra Horhocea <andra.hor@icloud.com>
2025-11-26 10:01:32 +0200

    feat: add StudentDto

---
+++ CmsWebApi/Cms.Repository/DTOs/StudentDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cms.Repository.DTOs;
+
+public class StudentDto
+{
+    public int StudentId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; }
+
+    [Required]
+    public string PhoneNumber { get; set; }
+
+    [MaxLength(50)]
+    public string Address { get; set; }
+}

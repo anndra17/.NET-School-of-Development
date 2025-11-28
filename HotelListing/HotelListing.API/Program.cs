ba12dfb893141d5b1c4493f27aabbd532ee98790
Andra Horhocea <andra.hor@icloud.com>
2025-11-28 18:52:19 +0200

    feat: configure EF connectionstring and options for DbContext

--- HotelListing/HotelListing.API/Program.cs
+++ HotelListing/HotelListing.API/Program.cs
@@ -1,9 +1,16 @@
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+
+builder.Services.AddDbContext<HotelListingDbContext>(options => {
+    options.UseSqlServer(connectionString);
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

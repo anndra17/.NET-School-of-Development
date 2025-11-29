04d37c496978d6afb5f9a0795e59eb2f82417a6c
Andra Horhocea <andra.hor@icloud.com>
2025-11-29 16:01:08 +0200

    feat: add CountriesRepository and inject DbContext into it

---
+++ HotelListing/HotelListing.API/Repository/CountriesRepository.cs
@@ -0,0 +1,13 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+
+namespace HotelListing.API.Repository;
+
+public class CountriesRepository : GenericRepository<Country>, ICountriesRepository
+{
+    private readonly HotelListingDbContext _context;
+    public CountriesRepository(HotelListingDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}

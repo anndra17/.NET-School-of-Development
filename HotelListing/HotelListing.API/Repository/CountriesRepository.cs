872b8d2792562fc24e7c18f8ff41ccb6f3c52009
Andra Horhocea <andra.hor@icloud.com>
2025-12-01 13:09:24 +0200

    style: add empty line before constructor

--- HotelListing/HotelListing.API/Repository/CountriesRepository.cs
+++ HotelListing/HotelListing.API/Repository/CountriesRepository.cs
@@ -7,6 +7,7 @@ namespace HotelListing.API.Repository;
 public class CountriesRepository : GenericRepository<Country>, ICountriesRepository
 {
     private readonly HotelListingDbContext _context;
+
     public CountriesRepository(HotelListingDbContext context) : base(context)
     {
         _context = context;

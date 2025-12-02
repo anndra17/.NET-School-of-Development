257afb9c893538e5c5d217c81d3e8b77e9f9ad44
Andra Horhocea <andra.hor@icloud.com>
2025-12-02 15:54:14 +0200

    feat: implement GenerateToken method

--- HotelListing/HotelListing.API/Repository/AuthManager.cs
+++ HotelListing/HotelListing.API/Repository/AuthManager.cs
@@ -3,7 +3,11 @@ using HotelListing.API.Contracts;
 using HotelListing.API.Data;
 using HotelListing.API.Models.Users;
 using Microsoft.AspNetCore.Identity;
-using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace HotelListing.API.Repository;
 
@@ -11,29 +15,31 @@ public class AuthManager : IAuthManager
 {
     private readonly IMapper _mapper;
     private readonly UserManager<ApiUser> _userManager;
+    private readonly IConfiguration _configuration;
 
-    public AuthManager(IMapper mapper, UserManager<ApiUser> userManager)
+    public AuthManager(IMapper mapper, UserManager<ApiUser> userManager, IConfiguration configuration)
     {
         _mapper = mapper;
         _userManager = userManager;
+        _configuration = configuration;
     }
 
-
-    public async Task<bool> Login(LoginDto loginDto)
+    public async Task<AuthResponseDto> Login(LoginDto loginDto)
     {
-        bool isValidUser;
+        var user = await _userManager.FindByEmailAsync(loginDto.Email);
+        bool isValidUser = await _userManager.CheckPasswordAsync(user, loginDto.Password);
 
-        try
+        if (user == null || isValidUser == false)
         {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
-            isValidUser = await _userManager.CheckPasswordAsync(user, loginDto.Password);
-        }
-        catch (Exception) 
-        {
-            throw;
+            return null;
         }
 
-        return isValidUser;
+        var token = await GenerateToken(user);
+        return new AuthResponseDto
+        {
+            Token = token,
+            UserId = user.Id
+        };
     }
 
     public async Task<IEnumerable<IdentityError>> Register(ApiUserDto userDto)
@@ -49,5 +55,35 @@ public class AuthManager : IAuthManager
         }
 
         return result.Errors;
+    }
+
+    public async Task<string> GenerateToken(ApiUser user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+
+        var credentials =  new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var roles = await _userManager.GetRolesAsync(user);
+        var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+        var userClaims = await _userManager.GetClaimsAsync(user);
+
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Email), // sub - subject
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim("uid", user.Id),
+        }
+        .Union(userClaims).Union(roleClaims);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["JwtSettings:Issuer"],
+            audience: _configuration["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(Convert.ToInt32(_configuration["JwtSettings:DurationInMinutes"])),
+            signingCredentials: credentials
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }

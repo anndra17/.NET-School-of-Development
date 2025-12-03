3f5d1cf0d0deb48a9851a67a49b579c355a249fb
Andra Horhocea <andra.hor@icloud.com>
2025-12-03 17:45:54 +0200

    feat: add QueryParameters

---
+++ HotelListing/HotelListing.API/Models/QueryParameters.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelListing.API.Models;
+
+public class QueryParameters
+{
+    private int _pageSize = 15;
+    public int StartIndex { get; set; } 
+    public int PageSize
+    {
+        get
+        {
+            return _pageSize;
+        }
+        set
+        {
+            _pageSize = value;
+        }
+    }
+}

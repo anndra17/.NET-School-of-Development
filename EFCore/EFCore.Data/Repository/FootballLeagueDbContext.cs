f5e520d7fcd8a1f5ecc61f4a947fb2ebff6ad9c6
Andra Horhocea <andra.hor@icloud.com>
2025-12-09 10:17:55 +0200

    refactor: change the path of .db file and add seeds with static DateTime

--- EFCore/EFCore.Data/Repository/FootballLeagueDbContext.cs
+++ EFCore/EFCore.Data/Repository/FootballLeagueDbContext.cs
@@ -1,20 +1,21 @@
 ﻿using EFCore.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace EFCore.Data.Repository;
 
 public class FootballLeagueDbContext : DbContext
 {
     private readonly string _dbPath;
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Coach> Coaches { get; set; }
 
     public FootballLeagueDbContext()
     {
-        var folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        _dbPath = Path.Combine(folder, "FootballLeague_EfCore.db");
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        _dbPath = Path.Combine(path, "FootballLeague_EfCore.db");
     }
-
-    public DbSet<Team> Teams { get; set; }
-    public DbSet<Coach> Coaches { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -24,5 +25,28 @@ public class FootballLeagueDbContext : DbContext
 
         // Using SQLite
         optionsBuilder.UseSqlite($"Data Source={_dbPath}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Team>().HasData(
+            new Team
+            {
+                Id = 1,
+                Name = "Tivoli Gardens FC",
+                CreatedDate = new DateTime(2024, 01, 01),
+            },
+            new Team
+            {
+                Id = 2,
+                Name = "Dinamo FC",
+                CreatedDate = new DateTime(2025, 04, 01),
+            },
+            new Team
+            {
+                Id = 3,
+                Name = "FCSDB",
+                CreatedDate = new DateTime(2004, 01, 01),
+            });
     }
 }

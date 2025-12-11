68623d53ba71ec387de271b746aa070cad1932e6
Andra Horhocea <andra.hor@icloud.com>
2025-12-11 15:26:20 +0200

    feat: add Match entity

---
+++ EFCore/EFCore.Domain/Models/Match.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCore.Domain.Models;
+
+public class Match
+{
+    public int HomeTeamId { get; set; }
+    public int AwayTeamId { get; set; }
+    public decimal TicketPrice { get; set; }
+    public DateTime Date { get; set; }
+}

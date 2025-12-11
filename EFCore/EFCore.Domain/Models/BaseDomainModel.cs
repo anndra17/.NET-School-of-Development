0b972674646239e4d5318e2d6c4a908d94590429
Andra Horhocea <andra.hor@icloud.com>
2025-12-11 15:36:09 +0200

    fix: make some properties optional

--- EFCore/EFCore.Domain/Models/BaseDomainModel.cs
+++ EFCore/EFCore.Domain/Models/BaseDomainModel.cs
@@ -4,7 +4,7 @@ public class BaseDomainModel
 {
     public int Id { get; set; }
     public DateTime CreatedDate { get; set; }
-    public DateTime ModifiedDate { get; set; }
-    public string CreatedBy { get; set; }
-    public string ModifiedBy { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    public string? CreatedBy { get; set; }
+    public string? ModifiedBy { get; set; }
 }

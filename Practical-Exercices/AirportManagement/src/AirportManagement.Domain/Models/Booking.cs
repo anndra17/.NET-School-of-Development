851324512d80a82d8730348fc4416382a5fda643
Andra Horhocea <andra.hor@icloud.com>
2025-12-19 15:46:35 +0200

    feat: add Booking model class

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Domain/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿namespace AirportManagement.Domain.Models;
+
+public class Booking
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public string ConfirmationCode { get; set; } = null!;
+
+    public int Quantity { get; set; }
+
+    public byte Status { get; set; }
+
+    public DateTime? CreatedUtc { get; set; }
+}

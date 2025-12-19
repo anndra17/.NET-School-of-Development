93b31c09df5960e008fb395482ac60a84cc43bfc
Andra Horhocea <andra.hor@icloud.com>
2025-12-19 18:15:08 +0200

    feat: add Gate repository

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/GateRepository.cs
@@ -0,0 +1,12 @@
+﻿using AirportManagement.Application.Abstractions;
+using AirportManagement.Domain.Models;
+using AirportManagement.Infrastructure.Persistence;
+
+namespace AirportManagement.Infrastructure.Repositories;
+
+public class GateRepository : GenericRepository<Gate>, IGateRepository
+{
+    public GateRepository(AirportManagementDbContext context) : base(context)
+    {
+    }
+}

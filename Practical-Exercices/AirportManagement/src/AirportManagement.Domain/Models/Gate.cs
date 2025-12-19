57db1549334631e78c24ff2b620c5a7400991483
Andra Horhocea <andra.hor@icloud.com>
2025-12-19 15:51:22 +0200

    feat: add Gate model class

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Domain/Models/Gate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportManagement.Domain.Models
+{
+    internal class Gate
+    {
+    }
+}

ce2bad35fbbcf1a54800f2d2bbe03ca0e3bcbd90
Andra Horhocea <andra.hor@icloud.com>
2025-12-19 15:42:57 +0200

    feat: add Airline model class

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Domain/Models/Airline.cs
@@ -0,0 +1,10 @@
+﻿namespace AirportManagement.Domain.Models;
+
+public class Airline
+{
+    public int Id { get; set; }
+
+    public string IATACode { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+}

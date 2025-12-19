8ff49b00496f0fa9f583370bcff2846ad7c31de4
Andra Horhocea <andra.hor@icloud.com>
2025-12-19 15:40:53 +0200

    feat: add Aircraft model class

--- Practical-Exercices/AirportManagement/src/AirportManagement.Domain/Models/Aircraft.cs
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Domain/Models/Aircraft.cs
@@ -2,5 +2,11 @@
 
 public class Aircraft
 {
+    public int Id { get; set; }
 
+    public string TailNumber { get; set; } = null!;
+
+    public string Model { get; set; } = null!;
+
+    public int SeatCapacity { get; set; }
 }

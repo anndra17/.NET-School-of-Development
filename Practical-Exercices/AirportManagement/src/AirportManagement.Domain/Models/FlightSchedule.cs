81cd5c775ee5d3cecddec16567c3b5e5e9af52df
Andra Horhocea <andra.hor@icloud.com>
2025-12-19 15:52:18 +0200

    feat: add FlightSchedule model class

--- Practical-Exercices/AirportManagement/src/AirportManagement.Domain/Models/FlightSchedule.cs
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Domain/Models/FlightSchedule.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace AirportManagement.Domain.Models;
 
-namespace AirportManagement.Domain.Models
+public class FlightSchedule
 {
-    internal class FlightSchedule
-    {
-    }
+    public int Id { get; set; }
+
+    public int FlightId { get; set; }
+
+    public DateTime ScheduledDepartureUtc { get; set; }
+
+    public DateTime ScheduledArrivalUtc { get; set; }
+
+    public int? GateId { get; set; }
+
+    public int? AssignedAircraftId { get; set; }
+
+    public byte Status { get; set; }
 }

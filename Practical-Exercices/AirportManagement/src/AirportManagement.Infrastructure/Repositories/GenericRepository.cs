eb259a0d5649b38409de1e70f7510a22cb3df9dc
Andra Horhocea <andra.hor@icloud.com>
2025-12-19 15:29:16 +0200

    feat: add implementation for GenericRepository into Infrastructure project

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using AirportManagement.Application.Abstractions;
+using AirportManagement.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AirportManagement.Infrastructure.Repositories;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+{
+    private readonly AirportManagementDbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public GenericRepository(AirportManagementDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task<TEntity?> GetByIdAsync(object Id)
+    {
+        return await _dbSet.FindAsync(Id);
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task InsertAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public async Task DeleteAsync(object Id)
+    {
+        var entity = await _dbSet.FindAsync(Id);
+        if (entity != null)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}

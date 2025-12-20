b0967f3e4ba40aae3d9509e3582a052f7567d666
Andra Horhocea <andra.hor@icloud.com>
2025-12-20 13:15:13 +0200

    feat: add FlightScheduleRepository class

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/FlightScheduleRepository.cs
@@ -0,0 +1,12 @@
+﻿using AirportManagement.Application.Abstractions;
+using AirportManagement.Domain.Models;
+using AirportManagement.Infrastructure.Persistence;
+
+namespace AirportManagement.Infrastructure.Repositories;
+
+public class FlightScheduleRepository : GenericRepository<FlightSchedule>, IFlightScheduleRepository
+{
+    public FlightScheduleRepository(AirportManagementDbContext context) : base(context)
+    {
+    }
+}

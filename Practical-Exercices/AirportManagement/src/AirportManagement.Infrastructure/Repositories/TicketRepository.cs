82585e429bc6d0f2c5f65162a3c1ea3dfc0733fb
Andra Horhocea <andra.hor@icloud.com>
2025-12-20 13:15:54 +0200

    feat: add TicketRepository class

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/TicketRepository.cs
@@ -0,0 +1,12 @@
+﻿using AirportManagement.Application.Abstractions;
+using AirportManagement.Domain.Models;
+using AirportManagement.Infrastructure.Persistence;
+
+namespace AirportManagement.Infrastructure.Repositories;
+
+public class TicketRepository : GenericRepository<Ticket>, ITicketRepository
+{
+    public TicketRepository(AirportManagementDbContext context) : base(context)
+    {
+    }
+}

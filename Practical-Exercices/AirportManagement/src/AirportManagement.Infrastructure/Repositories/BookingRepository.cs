f5ff30018d73becfdeae6a82daf2980fd09bc759
Andra Horhocea <andra.hor@icloud.com>
2025-12-20 13:14:12 +0200

    feat: add BookingRepository class

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using AirportManagement.Application.Abstractions;
+using AirportManagement.Domain.Models;
+using AirportManagement.Infrastructure.Persistence;
+
+namespace AirportManagement.Infrastructure.Repositories
+{
+    public class BookingRepository : GenericRepository<Booking>, IBookingRepository
+    {
+        public BookingRepository(AirportManagementDbContext context) : base(context)
+        {
+        }
+    }
+}

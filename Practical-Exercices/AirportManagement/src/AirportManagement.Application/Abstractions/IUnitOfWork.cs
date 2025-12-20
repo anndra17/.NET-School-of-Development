01f0f7bd8b070d572900456a302b2c2a4e142e0e
Andra Horhocea <andra.hor@icloud.com>
2025-12-20 13:17:18 +0200

    feat: add IUnitOfWork interface

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿namespace AirportManagement.Application.Abstractions;
+
+public interface IUnitOfWork
+{
+    IAircraftRepository Aircrafts { get; }
+    IAirlineRepository Airlines { get; }
+    IAirportRepository Airports { get; }
+    IBookingRepository Bookings { get; }
+    IFlightRepository Flights { get; }
+    IFlightScheduleRepository FlightSchedules { get; }
+    IGateRepository Gates { get; }
+    ITicketRepository Tickets { get; }
+    IUserRepository Users { get; }
+
+    Task SaveChangesAsync();
+}

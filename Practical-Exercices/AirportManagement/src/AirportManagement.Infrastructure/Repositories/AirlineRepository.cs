d4627767e1564254b5b9e185e062809e9fe3e250
Andra Horhocea <andra.hor@icloud.com>
2025-12-20 13:13:26 +0200

    feat: add AirlineRepository class

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/AirlineRepository.cs
@@ -0,0 +1,12 @@
+﻿using AirportManagement.Application.Abstractions;
+using AirportManagement.Domain.Models;
+using AirportManagement.Infrastructure.Persistence;
+
+namespace AirportManagement.Infrastructure.Repositories;
+
+public class AirlineRepository : GenericRepository<Airline>, IAirlineRepository
+{
+    public AirlineRepository(AirportManagementDbContext context) : base(context)
+    {
+    }
+}

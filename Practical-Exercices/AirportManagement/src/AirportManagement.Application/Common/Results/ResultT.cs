32d2b915d21090ba73ba5c85cc86bd6f3d166ebb
Andra Horhocea <andra.hor@icloud.com>
2025-12-22 15:03:43 +0200

    feat: add templated result to improve reusability

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Common/Results/ResultT.cs
@@ -0,0 +1,12 @@
+﻿using AirportManagement.Application.Enums;
+
+namespace AirportManagement.Application.Common.Results;
+
+public class Result<T> : Result
+{
+    public T? Value { get; init; }
+
+    public static Result<T> Ok(T value) => new() { Success = true, Value = value };
+    public new static Result<T> Fail(ErrorType type, string message) => new() { Success = false, ErrorType = type, ErrorMessage = message };
+
+}

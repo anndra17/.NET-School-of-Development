107e502140c111118a614e2ceeb88ee82eda01a0
Horhocea Andra <142351838+anndra17@users.noreply.github.com>
2025-12-22 18:12:59 +0200

    feat: add CreateFlightRequest DTO for flight creation
    Introduces the CreateFlightRequest dto to encapsulate flight creation details, including airline, flight number, origin, destination, optional aircraft, and active status.

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Dtos/Flight/CreateFlightRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace AirportManagement.Application.Dtos.Flight;
+
+public sealed class CreateFlightRequest
+{
+    public int AirlineId { get; set; }
+    public string FlightNumber { get; set; } = null!;
+    public int OriginAirportId { get; set; }
+    public int DestinationAirportId { get; set; }
+    public int? DefaultAircraftId { get; set; }
+    public bool? IsActive { get; set; }
+}

261afe8513398c10a3de04fd5ae42de9c46ec23a
Andra Horhocea <andra.hor@icloud.com>
2025-12-22 09:45:29 +0200

    feat: add FlightListItemResponse dto

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Dtos/Flight/FlightListItemResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace AirportManagement.Application.Dtos.Flight;
+
+public sealed class FlightListItemResponse
+{
+    public int Id { get; set; }
+    public string FlightNumber { get; set; } = null!;
+    public string? AirlineIataCode { get; set; }
+    public string? OriginIataCode { get; set; }
+    public string? DestinationIataCode { get; set; }
+    public bool IsActive { get; set; }
+}

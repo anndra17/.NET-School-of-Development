f4e81c612be70de32a47758d96b7cf5b9bfa46a9
Andra Horhocea <andra.hor@icloud.com>
2025-12-22 09:49:54 +0200

    feat: add extensio method to map from Domain.Flights to FlightResponse dto

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Mappings/FlightDtoMapper.cs
@@ -0,0 +1,19 @@
+﻿using AirportManagement.Application.Dtos.Flight;
+using AirportManagement.Domain.Models;
+
+namespace AirportManagement.Application.Mappings;
+
+internal static class FlightDtoMapper
+{
+    public static FlightResponseDto MapToFlightResponse(this Flight e)
+       => new()
+       {
+           Id = e.Id,
+           AirlineId = e.AirlineId,
+           FlightNumber = e.FlightNumber,
+           OriginAirportId = e.OriginAirportId,
+           DestinationAirportId = e.DestinationAirportId,
+           DefaultAircraftId = e.DefaultAircraftId,
+           IsActive = e.IsActive,
+       };
+}

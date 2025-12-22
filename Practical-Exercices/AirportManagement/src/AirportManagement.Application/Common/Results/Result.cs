1a8133be43f4c3726c9af4462b9acf09853adb2b
Andra Horhocea <andra.hor@icloud.com>
2025-12-22 15:01:37 +0200

    feat: add Result class to group errors in ErrorType

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Common/Results/Result.cs
@@ -0,0 +1,13 @@
+﻿using AirportManagement.Application.Enums;
+
+namespace AirportManagement.Application.Common.Results;
+
+public class Result
+{
+    public bool Success { get; init; }
+    public FlightErrorType? ErrorType { get; init; }
+    public string? ErrorMessage { get; init; }
+
+    public static Result Ok() => new() { Success = true };
+    public static Result Fail(FlightErrorType type, string message) => new() { Success = false, ErrorType = type, ErrorMessage = message };
+}

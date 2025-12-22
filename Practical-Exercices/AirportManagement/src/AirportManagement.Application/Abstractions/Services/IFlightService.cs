641c4043b8d159facf92ecb8edb58579ce0e6bf8
Andra Horhocea <andra.hor@icloud.com>
2025-12-22 09:46:58 +0200

    feat: add IFlightService with GetByIdAsync method

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Abstractions/Services/IFlightService.cs
@@ -0,0 +1,8 @@
+﻿using AirportManagement.Application.Dtos.Flight;
+
+namespace AirportManagement.Application.Abstractions.Services;
+
+public interface IFlightService
+{
+    Task<FlightResponseDto> GetByIdAsync(int id, CancellationToken ct);
+}

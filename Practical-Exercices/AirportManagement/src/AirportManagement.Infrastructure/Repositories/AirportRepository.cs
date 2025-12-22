c9173b557efce6b9c1398ffea4cbc090e17c425d
Horhocea Andra <142351838+anndra17@users.noreply.github.com>
2025-12-22 18:47:02 +0200

    feat: add ExistsAsync method to AirportRepository

--- Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/AirportRepository.cs
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/AirportRepository.cs
@@ -1,13 +1,17 @@
 ﻿using AirportManagement.Application.Abstractions.Repositories;
 using AirportManagement.Domain.Models;
 using AirportManagement.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace AirportManagement.Infrastructure.Repositories;
 
 public class AirportRepository : IAirportRepository
 {
+    private readonly AirportManagementDbContext _context;
+
     public AirportRepository(AirportManagementDbContext context) 
     {
+        _context = context;
     }
 
     public Task DeleteAsync(int Id, CancellationToken ct = default)
@@ -33,5 +37,10 @@ public class AirportRepository : IAirportRepository
     public Task UpdateAsync(Airport entity, CancellationToken ct = default)
     {
         throw new NotImplementedException();
+    }
+
+    public async Task<bool> ExistsAsync(int id, CancellationToken ct = default)
+    {
+        return await _context.Airports.AnyAsync(f => f.Id == id, ct);
     }
 }

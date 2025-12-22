0a2fb1438cfda007a2838461fe707b4123ba5848
Andra Horhocea <andra.hor@icloud.com>
2025-12-22 09:40:50 +0200

    feat: add PagedResponse<T> dto

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Dtos/PagedResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace AirportManagement.Application.Dtos;
+
+public sealed class PagedResponse<T>
+{
+    public IReadOnlyList<T> Items { get; init; } = Array.Empty<T>();
+    public int Page { get; init; }
+    public int PageSize { get; init; }
+    public int TotalItems { get; init; }
+    public int TotalPages { get; init; }
+}

dd921b6de7941c2d5dde92ee03c500fa8e31a444
Andra Horhocea <andra.hor@icloud.com>
2025-12-22 09:48:03 +0200

    feat: add implementation for GetByIdAsync method from FlightService class

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Services/FlightService.cs
@@ -0,0 +1,28 @@
+﻿using AirportManagement.Application.Abstractions.Repositories;
+using AirportManagement.Application.Abstractions.Services;
+using AirportManagement.Application.Dtos.Flight;
+using AirportManagement.Application.Mappings;
+
+namespace AirportManagement.Application.Services;
+
+public class FlightService : IFlightService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public FlightService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<FlightResponseDto?> GetByIdAsync(int id, CancellationToken ct)
+    {
+        var entity = await _unitOfWork.Flights.GetByIdAsync(id, ct);
+        
+        if (entity is null)
+        {
+            return null;
+        }
+
+        return entity.MapToFlightResponse();
+    }
+}

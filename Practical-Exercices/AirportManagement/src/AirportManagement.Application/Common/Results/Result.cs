6029b12341378924eb712167656f77fc30e5f79f
Horhocea Andra <142351838+anndra17@users.noreply.github.com>
2025-12-22 18:04:01 +0200

    refactor: rename FlightErrorType with ErrorType in Result class

--- Practical-Exercices/AirportManagement/src/AirportManagement.Application/Common/Results/Result.cs
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Common/Results/Result.cs
@@ -5,9 +5,9 @@ namespace AirportManagement.Application.Common.Results;
 public class Result
 {
     public bool Success { get; init; }
-    public FlightErrorType? ErrorType { get; init; }
+    public ErrorType? ErrorType { get; init; }
     public string? ErrorMessage { get; init; }
 
     public static Result Ok() => new() { Success = true };
-    public static Result Fail(FlightErrorType type, string message) => new() { Success = false, ErrorType = type, ErrorMessage = message };
+    public static Result Fail(ErrorType type, string message) => new() { Success = false, ErrorType = type, ErrorMessage = message };
 }

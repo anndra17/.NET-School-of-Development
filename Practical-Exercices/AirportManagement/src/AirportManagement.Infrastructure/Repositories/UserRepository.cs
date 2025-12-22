c1cd2f551568424dc70b2b112f2706e1bd5470ab
Horhocea Andra <142351838+anndra17@users.noreply.github.com>
2025-12-22 18:48:16 +0200

    feat: add ExistsAsync method to UserRepository

--- Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/UserRepository.cs
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Infrastructure/Repositories/UserRepository.cs
@@ -1,13 +1,17 @@
 ﻿using AirportManagement.Application.Abstractions.Repositories;
 using AirportManagement.Domain.Models;
 using AirportManagement.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace AirportManagement.Infrastructure.Repositories;
 
 public class UserRepository : IUserRepository
 {
+    private readonly AirportManagementDbContext _context;
+
     public UserRepository(AirportManagementDbContext context)
     {
+        _context = context;
     }
 
     public Task DeleteAsync(int Id, CancellationToken ct = default)
@@ -33,5 +37,9 @@ public class UserRepository : IUserRepository
     public Task UpdateAsync(User entity, CancellationToken ct = default)
     {
         throw new NotImplementedException();
+    }
+    public async Task<bool> ExistsAsync(int id, CancellationToken ct = default)
+    {
+        return await _context.Users.AnyAsync(f => f.Id == id, ct);
     }
 }

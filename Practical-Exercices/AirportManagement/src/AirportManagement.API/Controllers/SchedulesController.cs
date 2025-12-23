c8b219c8cd41b85dbb05484a7c2cb83b3a5fe6f8
Horhocea Andra <142351838+anndra17@users.noreply.github.com>
2025-12-23 02:09:51 +0200

    feat: add GetById method in SchedulesController

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.API/Controllers/SchedulesController.cs
@@ -0,0 +1,32 @@
+﻿using AirportManagement.Application.Abstractions.Services;
+using AirportManagement.Application.Dtos.Schedule;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirportManagement.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SchedulesController : ControllerBase
+{
+    private readonly IFlightScheduleService _scheduleService;
+
+    public SchedulesController(IFlightScheduleService scheduleService)
+    {
+        _scheduleService = scheduleService;
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(ScheduleResponseDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ScheduleResponseDto>> GetById(int id, CancellationToken ct)
+    {
+        var scheduleDto = await _scheduleService.GetByIdAsync(id, ct);
+
+        if (scheduleDto is null)
+        {
+            return NotFound(new { message = $"FlightSchedule {id} not found." });
+        }
+
+        return Ok(scheduleDto);
+    }
+}

70e45d0b26a6fb7ef54803d5b7791b70c2fd5db3
Horhocea Andra <142351838+anndra17@users.noreply.github.com>
2025-12-23 02:08:06 +0200

    feat: add IFlightScheduleService interface
    Introduces the IFlightScheduleService interface with a method for asynchronously retrieving a flight schedule by ID. This abstraction will support service implementations for flight schedule management.

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Abstractions/Services/IFlightScheduleService.cs
@@ -0,0 +1,9 @@
+﻿using AirportManagement.Application.Dtos.Flight;
+using AirportManagement.Application.Dtos.Schedule;
+
+namespace AirportManagement.Application.Abstractions.Services;
+
+public interface IFlightScheduleService
+{
+    Task<ScheduleResponseDto> GetByIdAsync(int id, CancellationToken ct);
+}

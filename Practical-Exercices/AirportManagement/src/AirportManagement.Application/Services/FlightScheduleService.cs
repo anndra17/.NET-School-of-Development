8017ce4de4325a49be2d6f3351ba39bfa4ae6ee6
Horhocea Andra <142351838+anndra17@users.noreply.github.com>
2025-12-23 02:08:24 +0200

    feat: add FlightScheduleService with GetByIdAsync method
    Introduces FlightScheduleService implementing IFlightScheduleService. Provides asynchronous retrieval of flight schedules by ID and maps results to ScheduleResponseDto.

---
+++ Practical-Exercices/AirportManagement/src/AirportManagement.Application/Services/FlightScheduleService.cs
@@ -0,0 +1,27 @@
+﻿using AirportManagement.Application.Abstractions.Repositories;
+using AirportManagement.Application.Abstractions.Services;
+using AirportManagement.Application.Dtos.Schedule;
+using AirportManagement.Application.Mappings;
+
+namespace AirportManagement.Application.Services;
+
+public class FlightScheduleService : IFlightScheduleService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public FlightScheduleService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<ScheduleResponseDto?> GetByIdAsync(int id, CancellationToken ct)
+    {
+        var schedule = await _unitOfWork.FlightSchedules.GetByIdAsync(id, ct);
+
+        if (schedule is null)
+        {
+            return null;
+        }
+
+        return schedule.MapToScheduleResponse();
+    }
+}
